2ec73627e20c95c8742eb215767aa1debc4a3fdb
iv-g <igarr001@odu.edu>
2024-02-26 16:32:56 -0500

    Refactored analysis logic into MarkdownSection

--- section.py
+++ section.py
@@ -11,6 +11,20 @@ class MarkdownSection:
         self.heading_level = heading_level
         self.raw_content = raw_content
 
+    def word_count(self):
+        return len(self.raw_content.split())
+
+    def sentence_count(self):
+        return len(self.raw_content.split('.'))
+
+    def paragraph_count(self):
+        return len(self.raw_content.strip().split('\n\n'))
+
     # Print string of an instance of the class
     def __str__(self):
-        return f"Header: {self.heading},\nHeading Level: {self.heading_level},\nRaw Content: {self.raw_content}"
+        return (f"Level {self.heading_level} Header: {self.heading}\n"
+                f"Words: {self.word_count()}\n"
+                f"Sentences: {self.sentence_count()}\n"
+                f"Paragraphs: {self.paragraph_count()}\n\n")
+
+        # return f"Header: {self.heading},\nHeading Level: {self.heading_level},\nRaw Content: {self.raw_content}"
