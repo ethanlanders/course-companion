823a24f7498e24c8610a26b3ecf33da44391b687
Alexji Gaston <agast002@odu.edu>
2024-03-22 11:56:32 -0400

    'Start of table creation'

--- CourseCompanionDatabse.py
+++ CourseCompanionDatabse.py
@@ -4,3 +4,20 @@ import sqlite3
 sqliteConnection = sqlite3.connect('CourseCompanion.db')
 
 cursor = sqliteConnection.cursor()
+
+#Creating tables (Needs further discussion)
+materials_table = ''' CREATE TABLE MATERIALS(
+    MATERIAL_ID = INT NOT NULL,
+    
+    
+)'''
+
+suggestions_table = ''' CREATE TABLE SUGGESTIONS(
+    
+)
+'''
+
+reports_table = ''' CREATE TABLE REPORTS(
+    
+)
+'''
