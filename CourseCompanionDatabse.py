acbcabb3c8917551d7bb7d3b978efa8327502c50
Alexji Gaston <agast002@odu.edu>
2024-03-27 13:16:54 -0400

    'adding to the tables'

--- CourseCompanionDatabse.py
+++ CourseCompanionDatabse.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import sqlite3
 
 #Creating the Database for the CourseCompanion
@@ -6,18 +7,25 @@ sqliteConnection = sqlite3.connect('CourseCompanion.db')
 cursor = sqliteConnection.cursor()
 
 #Creating tables (Needs further discussion)
+
 materials_table = ''' CREATE TABLE MATERIALS(
-    MATERIAL_ID = INT NOT NULL,
-    
-    
+    Material_id INT NOT NULL,
+    Course_name CHAR(50) NOT NULL,
+    Source_File,
+    Normalized_File, 
+    Date_last_analyzed datetime
 )'''
 
 suggestions_table = ''' CREATE TABLE SUGGESTIONS(
-    
+    suggestions_id INT NOT NULL,
+    entry_id INT NOT NULL,
+    suggesgtion_type
 )
 '''
 
 reports_table = ''' CREATE TABLE REPORTS(
-    
+    report_id INT NOT NULL,
+    Course_Name CHAR(50) NOT NULL,
+    entry_id INT,
 )
 '''
