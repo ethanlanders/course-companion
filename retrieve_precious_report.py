0fe6d6ae85ab23688d86a0122137727bbe7dd273
Alexji Gaston <agast002@odu.edu>
2024-04-10 13:45:22 -0400

    'Working on file retrieval'

---
+++ retrieve_precious_report.py
@@ -0,0 +1,38 @@
+import json
+import pickle
+import sys
+def retrieve_previous_report():
+    
+    #pickle.dump()
+    """
+    from datetime import datetime
+import sqlite3
+
+
+sqliteConnection = sqlite3.connect('CourseCompanion.db')
+
+cursor = sqliteConnection.cursor()
+
+
+materials_table = ''' CREATE TABLE MATERIALS(
+    Material_id INT NOT NULL,
+    Course_name CHAR(50) NOT NULL,
+    Source_File,
+    Normalized_File, 
+    Date_last_analyzed datetime
+)'''
+
+suggestions_table = ''' CREATE TABLE SUGGESTIONS(
+    suggestions_id INT NOT NULL,
+    entry_id INT NOT NULL,
+    suggesgtion_type
+)
+'''
+
+reports_table = ''' CREATE TABLE REPORTS(
+    report_id INT NOT NULL,
+    Course_Name CHAR(50) NOT NULL,
+    entry_id INT,
+)
+'''
+    """
